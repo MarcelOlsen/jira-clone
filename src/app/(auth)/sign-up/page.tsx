655d9a61ec3bfeb6457a32d33c813d0a038c79ab
MarcelOlsen <marcelolsen26@gmail.com>
2024-11-08 22:42:21 +0100

    Created auth routes

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { SingUpCard } from "@/features/auth/SignUpCard";
+import React from "react";
+
+const SignUpPage = () => {
+  return <SingUpCard />;
+};
+
+export default SignUpPage;
