87f207fdfcc918053617fe7df094decf4f76836d
MarcelOlsen <marcelolsen26@gmail.com>
2024-11-11 19:52:36 +0100

    Built the invite system

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -1,6 +1,7 @@
 import { zValidator } from "@hono/zod-validator"
 import { Hono } from "hono"
 import { ID, Query } from "node-appwrite"
+import { z } from "zod"
 
 import { sessionMiddleware } from "@/lib/session-middleware"
 import { generateInviteCode } from "@/lib/utils"
@@ -9,6 +10,7 @@ import { getMember } from "@/features/members/utils"
 import { DATABASE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config"
 
 import { createWorkspaceSchema, updateWorkspaceSchema } from "../schemas"
+import { Workspace } from "../types"
 
 const app = new Hono()
     .get("/", sessionMiddleware, async (c) => {
@@ -192,6 +194,47 @@ const app = new Hono()
                 workspaceId,
                 {
                     inviteCode: generateInviteCode(16)
+                }
+            )
+
+            return c.json({ data: workspace })
+        }
+    )
+    .post(
+        "/:workspaceId/join",
+        sessionMiddleware,
+        zValidator("json", z.object({ code: z.string() })),
+        async (c) => {
+            const { workspaceId } = c.req.param()
+            const { code } = c.req.valid("json")
+
+            const databases = c.get("databases")
+            const user = c.get("user")
+
+            const member = await getMember({
+                databases,
+                workspaceId,
+                userId: user.$id,
+            })
+
+            if (member) return c.json({ error: "Already a member " }, 400)
+
+            const workspace = await databases.getDocument<Workspace>(
+                DATABASE_ID,
+                WORKSPACES_ID,
+                workspaceId
+            )
+
+            if (workspace.inviteCode !== code) return c.json({ error: "Invalid invite code" }, 400)
+
+            await databases.createDocument(
+                DATABASE_ID,
+                MEMBERS_ID,
+                ID.unique(),
+                {
+                    workspaceId,
+                    userId: user.$id,
+                    role: MemberRole.MEMBER
                 }
             )
 
